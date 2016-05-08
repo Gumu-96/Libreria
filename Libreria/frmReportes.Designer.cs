2c43c4c2358c8af2425b30301d851e532e637540
GustavoMuller <gustavo.muller@programmer.net>
2016-05-07 19:27:27 -0600

    Reportes
    Primer reporte

--- Libreria/frmReportes.Designer.cs
+++ Libreria/frmReportes.Designer.cs
@@ -28,22 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnReporte = new MetroFramework.Controls.MetroTextBox.MetroTextButton();
             this.SuspendLayout();
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Image = null;
+            this.btnReporte.Location = new System.Drawing.Point(259, 192);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(145, 68);
+            this.btnReporte.Style = MetroFramework.MetroColorStyle.Green;
+            this.btnReporte.TabIndex = 0;
+            this.btnReporte.Text = "Generar reporte";
+            this.btnReporte.UseSelectable = true;
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
             // 
             // frmReportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.btnReporte);
             this.MaximizeBox = false;
             this.Name = "frmReportes";
             this.Sizable = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reportes";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmReportes_FormClosed);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTextBox.MetroTextButton btnReporte;
     }
 }
